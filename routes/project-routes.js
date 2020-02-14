5162e659ab2bee80f90d800731cfae5917068bde
Daniel Mattox <dmattox10@gmail.com>
2020-02-14 11:34:10 -0500

    Skeletal project routes

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -4,4 +4,37 @@ const Projects = require('../data/helpers/projectModel')
 
 const router = express.Router()
 
+router.post('/', validateProject, (res, res) => {
+
+})
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', validateId, (req, res) => {
+
+})
+
+router.get('/:id/actions', validateId, (req, res) => {
+    
+})
+
+router.put('/:id', validateId, (req, res) => {
+
+})
+
+router.delete('/:id', validateId, (req, res) => {
+
+})
+
+function validateProject(req, res, next) {
+
+    
+}
+
+function validateId(req, res, next) {
+
+}
+
 module.exports = router
