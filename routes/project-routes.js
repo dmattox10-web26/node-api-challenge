2cb604f7a586e81c9997666fc2a67e25602bc804
Daniel Mattox <dmattox10@gmail.com>
2020-02-14 11:49:52 -0500

    Project middleware complete

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -29,12 +29,33 @@ router.delete('/:id', validateId, (req, res) => {
 })
 
 function validateProject(req, res, next) {
-
+    if (req.body) {
+        // I've got a jar of dirt!
+        if (req.body.name && req.body.description) {
+            // savvy?
+            next()
+        }
+        else {
+            // Not good.
+            res.status(400).json({ message: 'missing one or more required fields' })
+        }
+    }
+    else {
+        // Nobody move! I dropped me brain!
+        res.status(400).json({ message: 'missing body' })
+    }
     
 }
 
 function validateId(req, res, next) {
-    
+    if(Projects.get(req.params.id)) {
+        // savvy?
+        next()
+    }
+    else {
+        // Not good.
+        res.status(404).json({ message: 'invalid id' })
+    }
 }
 
 module.exports = router
