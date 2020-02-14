7b1e01d18d66f1494bf114819a90f1ca07de70ca
Daniel Mattox <dmattox10@gmail.com>
2020-02-14 12:01:21 -0500

    Project routes complete

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -5,27 +5,63 @@ const Projects = require('../data/helpers/projectModel')
 const router = express.Router()
 
 router.post('/', validateProject, (res, res) => {
-
+    Projects.insert(req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
 })
 
 router.get('/', (req, res) => {
-
+    Projects.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
 })
 
 router.get('/:id', validateId, (req, res) => {
-
+    Projects.get(req.params.id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
 })
 
 router.get('/:id/actions', validateId, (req, res) => {
-
+    Projects.getProjectActions(req.params.id)
+    .then(actions => {
+        res.status(200).json(actions)
+    }) 
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
 })
 
 router.put('/:id', validateId, validateProject, (req, res) => {
-
+    Projects.update(req.params.id, req.body)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
 })
 
 router.delete('/:id', validateId, (req, res) => {
-
+    Projects.remove(req.params.id)
+    .then(removed => {
+        res.status(200).json(removed)
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
 })
 
 function validateProject(req, res, next) {
