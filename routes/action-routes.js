f10c7128ad07177e11c9a8d25b02da3fe43a8071
Daniel Mattox <dmattox10@gmail.com>
2020-02-14 11:31:12 -0500

    Skeletal action routes

--- routes/action-routes.js
+++ routes/action-routes.js
@@ -4,4 +4,33 @@ const Actions = require('../data/helpers/actionModel')
 
 const router = express.Router()
 
+router.post('/', validateAction, (res, res) => {
+
+})
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', validateId, (req, res) => {
+
+})
+
+router.put('/:id', validateId, (req, res) => {
+
+})
+
+router.delete('/:id', validateId, (req, res) => {
+
+})
+
+function validateAction(req, res, next) {
+
+    
+}
+
+function validateId(req, res, next) {
+
+}
+
 module.exports = router
